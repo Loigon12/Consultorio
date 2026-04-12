a3c5c7586bb3a556661797d63b7edda030943a2a
Juan Cossio <cossio0474@gmail.com>
2026-04-12 10:54:23 -0500

    update usuario

--- Backend/Usuari.py
+++ Backend/Usuari.py
@@ -1,18 +1,40 @@
 class Usuario:
 
-    def __init__(self, nombre, tipoDocumento, numeroDocumento, contraseña, correo, direccion = None, esAdmin = False):
+    def __init__(self, nombre, tipoDocumento, numeroDocumento, contraseña, correo, direccion = None, esAdmin = False, id = None):
         
 
-        self.id = None
+        self.__id = id
         self.__nombre = nombre
         self.__tipoDocumento = tipoDocumento
         self.__numeroDocumento = numeroDocumento
         self.__direccion = direccion
         self.__contraseña = contraseña
         self.__correo = correo
-        self.esAdmin = esAdmin
+        self.__esAdmin = esAdmin
+
+    def getId(self):
+        return self.__id
+
+    def getNombre(self):
+        return self.__nombre
     
-    def cambiarContraseña(self, contraseña, nuevaContraseña):
+    def getTipoDocumento(self):
+        return self.__tipoDocumento
+    
+    def getNumeroDocumento(self):
+        return self.__numeroDocumento
+
+    def getDireccion(self):
+        return self.__direccion
+
+    def getCorreo(self):
+        return self.__correo
+    
+    def getEsAdmin(self):
+        return self.__esAdmin
+    
+    
+    def __cambiarContraseña(self, contraseña, nuevaContraseña):
         if contraseña == self.__contraseña:
             self.__contraseña = nuevaContraseña
             return "Contraseña cambiada"
@@ -25,9 +47,11 @@ class Usuario:
     def editarDireccion(self, nuevaDireccion):
         self.__direccion = nuevaDireccion
 
+    
+
         
+prueba = Usuario("juan", "C:C", 1111, "1234", "name@gmail")
 
+prueba.__esAdmin = True
 
-prueba = Usuario("Juan", "C.C", 11111, "1234", "name@gmail.com", )
-
-print(prueba.nombre)
+print(prueba.__esAdmin)
